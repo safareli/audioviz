bba2ce4d971de43f712c05d7f2d008843c402daf
Safareli <i.safareli@gmail.com>
2014-04-14 20:46:58 +0000

    add test of random.color and random.object

--- test/random.js
+++ test/random.js
@@ -10,46 +10,71 @@ var timesGen = function(t,useArguuments){
     };
 };
 describe('random',function(){
+    var times = timesGen(100, false);
     describe('number',function(){
-        var times = timesGen(100, false);
+        var number = random.number;
         
         it('should return float from 0 to 1 with no arguments', times(function(){
-            expect(random.number()).to.be.within(0,1);
+            expect(number()).to.be.within(0,1);
         }));
         
         describe('should return a whole number if the round is true',function(){
             
             it('should return number within [0, arg1]', times(function(i){
-                var n = random.number(i);
+                var n = number(i);
                 expect(n).to.be.within(0,i);
                 expect(n).to.be.equal(n|0);
             }));
             
             it('should return number within [arg1, arg2]', times(function(i){
-                var n = random.number(i,i+10);
+                var n = number(i,i+10);
                 expect(n).to.be.within(i,i+10);
                 expect(n).to.be.equal(n|0);
             }));            
         });
         
-        describe('should return a whole number if the round is false',function(){
+        describe('should return number if the round is false',function(){
             
             it('should return number within [0, arg1]', times(function(i){
-                var n = random.number(i,false);
+                var n = number(i,false);
                 expect(n).to.be.within(0,i);
             }));
             
             it('should return number within [arg1, arg2]', times(function(i){
-                var n = random.number(i,i+10,false);
+                var n = number(i,i+10,false);
                 expect(n).to.be.within(i,i+10);
             }));            
         });
     });
     describe('object',function(){
-        it('should make random objcet with default keys');
+        var object = random.object;
+        
+        it('should make random object with default keys', times(function(i){
+            var position = object({
+                x: [-50*i, 50*i],
+                y: [-20*i, 20*i]
+            });
+            expect(position.x).to.be.within(-50*i, 50*i);
+            expect(position.y).to.be.within(-20*i, 20*i);
+        }));
     });
     describe('color',function(){
-        it('should extend default keys and return color object');
+        var color = random.color;
+        
+        it('should make random rgba color with no arguments', times(function(i){
+            var background = color();
+
+            expect(background.r).to.be.within(0,255);
+            expect(background.r).to.be.equal(background.r|0);
+            
+            expect(background.g).to.be.within(0,255);
+            expect(background.g).to.be.equal(background.g|0);
+            
+            expect(background.b).to.be.within(0,255);
+            expect(background.b).to.be.equal(background.b|0);
+            
+            expect(background.a).to.be.within(0,1);
+        }));
     });
     
 });
